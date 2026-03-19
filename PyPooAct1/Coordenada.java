937bb50b83e38acbbe8c9255daf4912aeb75e7f0
Angel Fernando Cruz Torres <davidasdadad@gmail.com>
2026-03-19 07:33:39 -0500

    Sesion01

---
+++ PyPooAct1/Coordenada.java
@@ -0,0 +1,61 @@
+package PyPooAct1;
+
+public class Coordenada {
+    private double x;
+    private double y;
+
+    // Constructor por defecto
+    public Coordenada() {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    // Constructor con parámetros
+    public Coordenada(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // Constructor copia
+    public Coordenada(Coordenada c) {
+        this.x = c.x;
+        this.y = c.y;
+    }
+
+    // Setters
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    // Getters
+    public double getX() {
+        return this.x;
+    }
+
+    public double getY() {
+        return this.y;
+    }
+
+    // Método de instancia
+    public double distancia(Coordenada c) {
+        double dx = this.x - c.x;
+        double dy = this.y - c.y;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    // Método de clase
+    public static double distancia(Coordenada c1, Coordenada c2) {
+        double dx = c1.x - c2.x;
+        double dy = c1.y - c2.y;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.x + ", " + this.y + "]";
+    }
+}
