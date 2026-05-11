9a388d5869d1b0c8addf051a1ad2f1dfdab74419
Alvaro Fabrizio Montoya Magallanes <164426724+Vadr0@users.noreply.github.com>
2026-05-11 07:27:40 -0500

    Create BinarySearchTree.java

---
+++ AED_LAB07/Actividades/BinarySearchTree.java
@@ -0,0 +1,19 @@
+package bstreeInterface;
+
+public interface BinarySearchTree<E extends Comparable<E>> {
+
+    void insert(E data);
+
+    boolean search(E data);
+
+    void delete(E data);
+
+    boolean isEmpty();
+
+    void destroy();
+
+    @Override
+    String toString();
+
+}
+
