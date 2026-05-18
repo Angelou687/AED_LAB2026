7fa2a161d591f0a9cf9d2477ab8a8fb1987acb53
Alvaro Fabrizio Montoya Magallanes <164426724+Vadr0@users.noreply.github.com>
2026-05-18 00:12:07 -0500

    Create NodeAVL.java

---
+++ AEB_LAB08/Actividades/Actividad 2.4/NodeAVL.java
@@ -0,0 +1,200 @@
+package Actividad_2;
+
+import Node.Node;
+import exceptions.ItemDuplicated;
+import Actividad_1.LinkedBST;
+
+public class AVLTree<E extends Comparable<E>> extends LinkedBST<E> {
+
+    protected static class NodeAVL<E> extends Node<E> {
+
+        int bf; // Factor de balance: -1, 0, 1
+
+        public NodeAVL(E data) {
+            super(data);
+            this.bf = 0;
+        }
+
+        @Override
+        public String toString() {
+            return data + "(bf:" + bf + ")";
+        }
+    }
+
+    private boolean height; // Indicador de cambio de altura
+
+    @Override
+    public void insert(E x) throws ItemDuplicated {
+        this.height = false;
+        this.root = insert(x, (NodeAVL<E>) this.root);
+    }
+
+    protected NodeAVL<E> insert(E x, NodeAVL<E> node) throws ItemDuplicated {
+        if (node == null) {
+            this.height = true;
+            return new NodeAVL<>(x);
+        }
+
+        int resC = x.compareTo(node.data);
+
+        if (resC == 0) {
+            throw new ItemDuplicated(x + " ya se encuentra en el árbol");
+        }
+
+        if (resC < 0) {
+            // Inserción por izquierda
+            node.left = insert(x, (NodeAVL<E>) node.left);
+
+            if (this.height) {
+                switch (node.bf) {
+                    case 1:
+                        node.bf = 0;
+                        this.height = false;
+                        break;
+
+                    case 0:
+                        node.bf = -1;
+                        this.height = true;
+                        break;
+
+                    case -1:
+                        // bf = -2, necesita balanceo
+                        node = balanceToRight(node);
+                        this.height = false;
+                        break;
+                }
+            }
+
+        } else {
+            // Inserción por derecha
+            node.right = insert(x, (NodeAVL<E>) node.right);
+
+            if (this.height) {
+                switch (node.bf) {
+                    case -1:
+                        node.bf = 0;
+                        this.height = false;
+                        break;
+
+                    case 0:
+                        node.bf = 1;
+                        this.height = true;
+                        break;
+
+                    case 1:
+                        // bf = 2, necesita balanceo
+                        node = balanceToLeft(node);
+                        this.height = false;
+                        break;
+                }
+            }
+        }
+
+        return node;
+    }
+
+    // Rotación Simple a Izquierda (RSL)
+    private NodeAVL<E> rotateSL(NodeAVL<E> node) {
+        NodeAVL<E> p = (NodeAVL<E>) node.right;
+
+        node.right = p.left;
+        p.left = node;
+
+        return p;
+    }
+
+    // Rotación Simple a Derecha (RSR)
+    private NodeAVL<E> rotateSR(NodeAVL<E> node) {
+        NodeAVL<E> q = (NodeAVL<E>) node.left;
+
+        node.left = q.right;
+        q.right = node;
+
+        return q;
+    }
+
+    // Balanceo hacia izquierda, para bf = +2
+    private NodeAVL<E> balanceToLeft(NodeAVL<E> node) {
+        NodeAVL<E> hijo = (NodeAVL<E>) node.right;
+
+        switch (hijo.bf) {
+            case 1:
+                // Rotación simple a izquierda
+                node.bf = 0;
+                hijo.bf = 0;
+                node = rotateSL(node);
+                break;
+
+            case -1:
+                // Rotación doble derecha-izquierda
+                NodeAVL<E> nieto = (NodeAVL<E>) hijo.left;
+
+                switch (nieto.bf) {
+                    case -1:
+                        node.bf = 0;
+                        hijo.bf = 1;
+                        break;
+
+                    case 0:
+                        node.bf = 0;
+                        hijo.bf = 0;
+                        break;
+
+                    case 1:
+                        node.bf = -1;
+                        hijo.bf = 0;
+                        break;
+                }
+
+                nieto.bf = 0;
+                node.right = rotateSR(hijo);
+                node = rotateSL(node);
+                break;
+        }
+
+        return node;
+    }
+
+    // Balanceo hacia derecha, simétrico a balanceToLeft
+    private NodeAVL<E> balanceToRight(NodeAVL<E> node) {
+        NodeAVL<E> hijo = (NodeAVL<E>) node.left;
+
+        switch (hijo.bf) {
+            case -1:
+                // Rotación simple a derecha
+                node.bf = 0;
+                hijo.bf = 0;
+                node = rotateSR(node);
+                break;
+
+            case 1:
+                // Rotación doble izquierda-derecha
+                NodeAVL<E> nieto = (NodeAVL<E>) hijo.right;
+
+                switch (nieto.bf) {
+                    case 1:
+                        node.bf = 0;
+                        hijo.bf = -1;
+                        break;
+
+                    case 0:
+                        node.bf = 0;
+                        hijo.bf = 0;
+                        break;
+
+                    case -1:
+                        node.bf = 1;
+                        hijo.bf = 0;
+                        break;
+                }
+
+                nieto.bf = 0;
+                node.left = rotateSL(hijo);
+                node = rotateSR(node);
+                break;
+        }
+
+        return node;
+    }
+}
+
